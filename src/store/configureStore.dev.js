cf7ded487d86452f3e2a999d9a151ab0a66cfd05
Rebecca Yi <rebeccayeyi@gmail.com>
2016-12-13 10:44:34 -0500

    heroku

--- src/store/configureStore.dev.js
+++ src/store/configureStore.dev.js
@@ -1,23 +1,23 @@
-// import { createStore, applyMiddleware, compose } from 'redux';
-// import thunk from 'redux-thunk'
-// import rootReducer from '../reducers/rootReducer'
-//
-// export default function configureStore(initialState) {
-//   const finalCreateStore = compose(
-//     applyMiddleware(thunk),
-//     // maybe use promises here?
-//     window.devToolsExtension ? window.devToolsExtension() : f => f
-//     )(createStore);
-//
-//     const store = finalCreateStore(rootReducer, initialState);
-//
-//     if (module.hot) {
-//       module.hot.accept('../reducers/rootReducer', () => {
-//         const nextReducer = require('../reducers/rootReducer');
-//       // might just be reducers instead of rootReducer?
-//         store.replaceReducer(nextReducer);
-//       })
-//     }
-//
-//     return store
-// }
+import { createStore, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk'
+import rootReducer from '../reducers/rootReducer'
+
+export default function configureStore(initialState) {
+  const finalCreateStore = compose(
+    applyMiddleware(thunk),
+    // maybe use promises here?
+    window.devToolsExtension ? window.devToolsExtension() : f => f
+    )(createStore);
+
+    const store = finalCreateStore(rootReducer, initialState);
+
+    if (module.hot) {
+      module.hot.accept('../reducers/rootReducer', () => {
+        const nextReducer = require('../reducers/rootReducer');
+      // might just be reducers instead of rootReducer?
+        store.replaceReducer(nextReducer);
+      })
+    }
+
+    return store
+}
