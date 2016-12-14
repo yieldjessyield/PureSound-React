3c363aabfb8c5dce022abbcfe1fbc8f7e358cdd2
Rebecca Yi <rebeccayeyi@gmail.com>
2016-12-14 17:14:50 -0500

    changed instructions

--- src/components/HelpBar.js
+++ src/components/HelpBar.js
@@ -12,9 +12,8 @@ export default function HelpBar() {
         <img className="helpIcons" src={require("../images/upArrow.svg")}/>&nbsp;&nbsp;p r e s s&nbsp; t h e&nbsp; u p&nbsp; a r r o w&nbsp; t o&nbsp; h i d e&nbsp; s o n g s<br/><br/>
         <img className="helpIcons" src={require("../images/downArrow.svg")}/>&nbsp;&nbsp;p r e s s&nbsp; t h e&nbsp; d o w n&nbsp; a r r o w&nbsp; t o&nbsp; s h o w&nbsp; s o n g s&nbsp; o r&nbsp; c l i c k&nbsp; t h e&nbsp; a r t i s t&nbsp; p h o t o<br/><br/>
         <img className="helpIcons" src={require("../images/click.svg")}/>&nbsp;&nbsp;c l i c k&nbsp; t h e&nbsp; s o n g&nbsp; t o&nbsp; p l a y / p a u s e<br/><br/>
-        <img className="helpIcons" src={require("../images/doubleClick.svg")}/>&nbsp;&nbsp;d o u b l e&nbsp; c l i c k&nbsp; t h e&nbsp; s o n g&nbsp; t o&nbsp; s h o w&nbsp; i t s&nbsp; t i t l e<br/><br/>
+        <img className="helpIcons" src={require("../images/doubleClick.svg")}/>&nbsp;&nbsp;h o v e r&nbsp;o v e r&nbsp; t h e&nbsp; s o n g&nbsp; t o&nbsp; s h o w&nbsp; i t s&nbsp; t i t l e<br/><br/>
       </div>
       <div className='yourLikedArtists'>help</div>
     </div>
     )}
-
